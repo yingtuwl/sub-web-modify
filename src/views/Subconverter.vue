a12d99ed781c3fd4e857ba2a793babaacf6c98cd
yingtuwl <85736153+yingtuwl@users.noreply.github.com>
2025-07-28 14:01:26 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -495,6 +495,39 @@ export default {
         ],
         remoteConfig: [
           {
+            label: "CM规则",
+            options: [
+              {
+                label: "CM_Online 默认版 识别港美地区(与Github同步)",
+                value: "https://raw.githubusercontent.com/cmliu/ACL4SSR/refs/heads/main/Clash/config/ACL4SSR_Online.ini"
+              },
+              {
+                label: "CM_Online_MultiCountry 识别港美地区 负载均衡(与Github同步)",
+                value: "https://raw.githubusercontent.com/cmliu/ACL4SSR/refs/heads/main/Clash/config/ACL4SSR_Online_MultiCountry.ini"
+              },
+              {
+                label: "CM_Online_MultiCountry_CF 识别港美地区、CloudFlareCDN 负载均衡 Worker节点专用(与Github同步)",
+                value: "https://raw.githubusercontent.com/cmliu/ACL4SSR/refs/heads/main/Clash/config/ACL4SSR_Online_MultiCountry_CF.ini"
+              },
+              {
+                label: "CM_Online_Full 识别多地区分组(与Github同步)",
+                value: "https://raw.githubusercontent.com/cmliu/ACL4SSR/refs/heads/main/Clash/config/ACL4SSR_Online_Full.ini"
+              },
+              {
+                label: "CM_Online_Full_CF 识别多地区、CloudFlareCDN 分组 Worker节点专用(与Github同步)",
+                value: "https://raw.githubusercontent.com/cmliu/ACL4SSR/refs/heads/main/Clash/config/ACL4SSR_Online_Full_CF.ini"
+              },
+              {
+                label: "CM_Online_Full_MultiMode 识别多地区 负载均衡(与Github同步)",
+                value: "https://raw.githubusercontent.com/cmliu/ACL4SSR/refs/heads/main/Clash/config/ACL4SSR_Online_Full_MultiMode.ini"
+              },
+              {
+                label: "CM_Online_Full_MultiMode_CF 识别多地区、CloudFlareCDN 负载均衡 Worker节点专用(与Github同步)",
+                value: "https://raw.githubusercontent.com/cmliu/ACL4SSR/refs/heads/main/Clash/config/ACL4SSR_Online_Full_MultiMode_CF.ini"
+              }
+            ]
+          },			
+          {
             label: "通用",
             options: [
               {
