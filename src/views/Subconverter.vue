9cc75012d92c65a76e008d046ace0142846f1677
yingtuwl <85736153+yingtuwl@users.noreply.github.com>
2025-08-04 18:57:42 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -474,7 +474,7 @@ export default {
           "sub.cm": "https://sub.cm/short",
         },
         customBackend: {
-	  "影途增强型后端": "https://suburl-api.1881999.xyz/version",
+	  "影途增强型后端": "https://suburl-api.1881999.xyz",
 	  "CM负载均衡后端【vless reality+hy1+hy2】": "https://subapi.cmliussss.net",
 	  "CM备用后端【vless reality+hy1+hy2】": "https://subapi.fxxk.dedyn.io",
           "肥羊增强型后端【vless reality+hy1+hy2】": "https://url.v1.mk",
@@ -485,7 +485,7 @@ export default {
           "sub作者&lhie1提供": "https://api.dler.io",
         },
         backendOptions: [
-	  {value: "https://suburl-api.1881999.xyz/version"},
+	  {value: "https://suburl-api.1881999.xyz"},
           {value: "https://subapi.cmliussss.net"},
           {value: "https://subapi.fxxk.dedyn.io"},
           {value: "https://url.v1.mk"},
@@ -1451,4 +1451,5 @@ export default {
   }
 };
 </script>
+
 
